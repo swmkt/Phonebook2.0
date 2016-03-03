fcd1bfe76f03cb196e5fd4dc757a69b6bd8fabc8
Yulia Orlova <julia.a.orlova@gmail.com>
2016-03-03 20:45:30 +0200

    Filters, sort
    Implemented:
    1) Multi filters on grid
    2) Sorting by 1 column

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ var fakeContacts = [
         "gender": "Male",
         "sign": "Leo",
         "notes": "Some notes"},
-    {"phone": "0555666",
+    {"phone": "0",
+        "name": "Test Name 2",
+        "city": "World",
+        "gender": "Female",
+        "sign": "Cancer",
+        "notes": "Some notes"},
+    {"phone": "333",
         "name": "Test Name 2",
         "city": "World",
         "gender": "Female",
@@ -14,18 +20,20 @@ var fakeContacts = [
 ];
 var emptyContacts = [];
 localStorage.setItem("contacts", JSON.stringify(fakeContacts));
-console.log("1", localStorage.getItem('contacts'));
 
 
 var ContactViewModel = function() {
     var self = this;
-    ko.mapping.fromJSON(JSON.parse(localStorage.getItem('contacts')), {}, this.contacts);
+    //ko.mapping.fromJSON(JSON.parse(localStorage.getItem('contacts')), {}, this.contacts);
     self.contacts = ko.observableArray(fakeContacts);
     self.editMode = ko.observable(false);
-    self.selectedContact = ko.observable();
-    
+    self.selectedContact = ko.observable(); 
     self.signOptions = ['Leo', 'Cancer', 'Scorpion'];
-    
+    self.currentPhoneFilter = ko.observable("");
+    self.currentNameFilter = ko.observable("");
+    self.currentCityFilter = ko.observable("");
+    self.currentGenderFilter = ko.observable("");
+
     self.editContact = function(contact) {
         self.editMode(true);
         self.contacts.remove(contact);
@@ -43,6 +51,45 @@ var ContactViewModel = function() {
     self.removeContact = function(contact) {
         self.contacts.remove(contact);
     };
+    self.sortContactsBy = function()  {
+        self.contacts.sort(function(left, right) { return left.phone == right.phone ? 0 : (left.phone < right.phone ? -1 : 1) });
+    };
+    self.sortContactsBy = function(fieldName, ascending)  {
+        self.contacts.sort(function(a, b) {
+            if (a[fieldName] > b[fieldName]) {
+                return ascending ? +1 : -1;
+            }
+            if (a[fieldName] < b[fieldName]) {
+                return ascending ? -1 : +1;
+            }
+            return 0;
+        });
+    }
+    self.filterContacts = ko.computed(function() {
+        var tempContacts = self.contacts();
+        if (self.currentPhoneFilter()) {
+            tempContacts = _.filter(self.contacts(), function(contact){ return contact.phone.toLowerCase().indexOf(self.currentPhoneFilter().toLowerCase()) >= 0; });
+        }
+        if (self.currentNameFilter()) {
+            tempContacts = _.filter(tempContacts, function(contact){ return contact.name.toLowerCase().indexOf(self.currentNameFilter().toLowerCase()) >= 0; });
+        }
+        if (self.currentCityFilter()) {
+            tempContacts = _.filter(tempContacts, function(contact){ return contact.city.toLowerCase().indexOf(self.currentCityFilter().toLowerCase()) >= 0; });
+        }
+        if (self.currentGenderFilter()) {
+            tempContacts = _.filter(tempContacts, function(contact){ return contact.gender.toLowerCase().indexOf(self.currentGenderFilter().toLowerCase()) >= 0; });
+        }
+
+        return tempContacts;
+    }, ContactViewModel);
+
+    function filterBy(query){
+        if (!query) {
+            return self.contacts();
+        } else {
+            return _.filter(self.contacts(), function(contact){ return contact.phone.toLowerCase().indexOf(query.toLowerCase()) >= 0; });
+        }
+    }
 };
 
 ko.applyBindings(new ContactViewModel());
